fff28c60032f2406f16dc696fb227e0b6de14322
Gustavo M <gusmaldonadob@gmail.com>
2021-04-01 18:19:43 -0600

    folder creation

---
+++ Documents/Python_Scripts/First_name.py
@@ -0,0 +1,2 @@
+your_first_name = input("What is your name ")
+neighbor_first_name = input("What is your neighbor's name ")
---
+++ Documents/Python_Scripts/Rock_Paper_Scissors.py
@@ -0,0 +1,20 @@
+# Incorporate the random library
+import random
+
+# Print Title
+print("Let's Play Rock Paper Scissors!")
+
+# Specify the three options
+options = ["r", "p", "s"]
+
+# Computer Selection
+computer_choice = random.choice(r,p,s)
+
+# User Selection
+user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+
+# Run Conditionals
+if (user_choise == "r" an computer_choice == "p"):
+    print("You chose rock. The computer chose paper")
+    print("You won")
+
